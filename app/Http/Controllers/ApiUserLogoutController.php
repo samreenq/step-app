fa2ba1cc2ddd38bf666b50e4940bc1748265d796
samreen <samreen.quyyum@cubixlabs.com>
2019-10-25 18:37:38 +0500

    User Logout  - update status

--- app/Http/Controllers/ApiUserLogoutController.php
+++ app/Http/Controllers/ApiUserLogoutController.php
@@ -22,7 +22,7 @@
 
                 $user_data->device_token = '';
                 $user_data->save();
-				$this->output(makeClientHappy($user_data));
+				$this->output(makeClientHappy([],'success'));
 
 		    }
 
@@ -35,5 +35,28 @@
 		        //This method will be execute after run the main process
 
 		    }
+
+            /**
+             * @param string $output
+             * @return \Illuminate\Http\JsonResponse
+             */
+            public function execute_api($output = 'API')
+            {
+                try{
+                    $result = parent::execute_api();
+                    $api_response = isset($result->original) ? $result->original : [];
+                    $response = apiResponse($api_response);
+
+                    if($api_response['api_status'] == 1){
+                        return response()->json($response, 200);
+                    }
+                    return response()->json($response, 400);
+                }
+                catch (\Exception $e){
+                    $response['message'] = $e->getMessage();
+                    // $response['trace'] = $e->getTraceAsString();
+                    return response()->json($response,400);
+                }
+            }
 
 		}
