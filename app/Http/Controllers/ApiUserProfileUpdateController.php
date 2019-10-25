25096ea29e4971b1192d71a1111e98e0489cbf9d
samreen <samreen.quyyum@cubixlabs.com>
2019-10-25 17:37:05 +0500

    User Profile Update - update response and send user data

--- app/Http/Controllers/ApiUserProfileUpdateController.php
+++ app/Http/Controllers/ApiUserProfileUpdateController.php
@@ -4,6 +4,7 @@
 		use Request;
 		use DB;
 		use CRUDBooster;
+		use App\AppUser;
 
 		class ApiUserProfileUpdateController extends \crocodicstudio\crudbooster\controllers\ApiController {
 
@@ -18,7 +19,6 @@
 		    public function hook_before(&$postdata) {
 				$postdata = removeEmptyKeys($postdata);
 				$postdata = filterPostRequest($postdata , $this->table);
-
 		    }
 
 		    public function hook_query(&$query) {
@@ -27,13 +27,35 @@
 		    }
 
 		    public function hook_after($postdata,&$result) {
+		        //dd($result);
 				if($result['api_status']) {
-
-					$user_data = $this->user_model->find($result['id']);
-
+                    $user_data = $this->user_model->with('token')->find($postdata['id']);
 					$result =  makeClientHappy($user_data);
 				}
 
 		    }
+
+            /**
+             * @param string $output
+             * @return \Illuminate\Http\JsonResponse
+             */
+            public function execute_api($output = 'API')
+            {
+                try{
+                    $result = parent::execute_api();
+                    $api_response = isset($result->original) ? $result->original : [];
+                    $response = apiResponse($api_response);
+
+                    if($api_response['api_status'] == 1){
+                        return response()->json($response, 200);
+                    }
+                    return response()->json($response, 400);
+                }
+                catch (\Exception $e){
+                    $response['message'] = $e->getMessage();
+                    // $response['trace'] = $e->getTraceAsString();
+                    return response()->json($response,400);
+                }
+            }
 
 		}
