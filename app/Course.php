96c53ffa7fa65ec231f87516a1c7e1e89853a03f
samreen <samreen.quyyum@cubixlabs.com>
2019-10-29 18:56:59 +0500

    Get Courses - count lesson and update response by course id

--- app/Course.php
+++ app/Course.php
@@ -9,7 +9,8 @@ class Course extends Model
     //
     protected $appends = [
         'avg_rating',
-        'icon_path'
+        'icon_path',
+        'total_lesson'
     ];
 
     /**
@@ -19,13 +20,24 @@ class Course extends Model
     {
 
         $reviews_model = new Review();
-        $rating = $reviews_model->getRatingByCourse($this->id);
-        return $rating;
+        return $reviews_model->getRatingByCourse($this->id);
     }
 
     public function getIconPathAttribute()
     {
 
         return url('/').'/'.$this->icon;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTotalLessonAttribute()
+    {
+
+        $model = new Lesson();
+       return $model->where('course_id',$this->id)
+           ->where('is_active',1)
+           ->whereNull('deleted_at')->count();
     }
 }
