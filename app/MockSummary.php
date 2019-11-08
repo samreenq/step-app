e158a32cff5211c8e1e6205891b2b7fdb0f76f77
samreen <samreen.quyyum@cubixlabs.com>
2019-11-08 19:45:35 +0500

    Get Mock Summary

---
+++ app/MockSummary.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MockSummary extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'mock_summary';
+
+    protected $fillable = [ 'title','description','is_active'];
+
+
+}
