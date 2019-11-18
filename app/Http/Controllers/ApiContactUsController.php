6960dd105fa3e000dad23bdc8ee81aa1dbdad014
samreen <samreen.quyyum@cubixlabs.com>
2019-11-18 15:49:10 +0500

    Contact Us - update the message

--- app/Http/Controllers/ApiContactUsController.php
+++ app/Http/Controllers/ApiContactUsController.php
@@ -12,12 +12,13 @@
 				$this->permalink   = "contact_us";    
 				$this->method_type = "post";    
 		    }
-		
+
 
 		    public function hook_before(&$postdata) {
 		        unset($postdata['user_id']);
 
 		        //This method will be execute before run the main process
+               // $this->user_model->sendRegisterMail($user_data);
 
 		    }
 
@@ -28,7 +29,31 @@
 
 		    public function hook_after($postdata,&$result) {
 		        //This method will be execute after run the main process
-
+                $this->output(sendErrorToClient('Thank you for contact, we will reach you soon.'));
 		    }
+
+            /**
+             * @param string $output
+             * @return \Illuminate\Http\JsonResponse
+             */
+            public function execute_api($output = 'API')
+            {
+                try{
+                    $result = parent::execute_api();
+                    //echo '<pre>'; print_r($result->original); exit;
+                    $api_response = isset($result->original) ? $result->original : [];
+                    $response = apiResponse($api_response);
+
+                    if($api_response['api_status'] == 1){
+                        return response()->json($response, 200);
+                    }
+                    return response()->json($response, 400);
+                }
+                catch (\Exception $e){
+                    $response['message'] = $e->getMessage();
+                    // $response['trace'] = $e->getTraceAsString();
+                    return response()->json($response,400);
+                }
+            }
 
 		}
