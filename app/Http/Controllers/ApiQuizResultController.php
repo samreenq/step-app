e9fc9f7a662541f20843e32d5a5bee6737320dab
samreen <samreen.quyyum@cubixlabs.com>
2019-11-20 17:30:55 +0500

    Quiz Result - Calculate

--- app/Http/Controllers/ApiQuizResultController.php
+++ app/Http/Controllers/ApiQuizResultController.php
@@ -15,27 +15,47 @@
 				$this->table       = "quiz_result";        
 				$this->permalink   = "quiz_result";    
 				$this->method_type = "post";
+				$this->model = new QuizResult();
 		    }
 		
 
 		    public function hook_before(&$postdata) {
-		        //delete if already exists
-                QuizResult::where('lesson_id',$postdata['lesson_id'])->where('user_id',$postdata['user_id'])->delete();
-		        $count_wrong = substr_count($postdata['is_correct'],0);
-		        $count_correct = substr_count($postdata['is_correct'],1);
-		        $count_questions = Quiz::where('lesson_id',$postdata['lesson_id'])->count();
-		        $passing_score = Setting::where('name','passing_score')->first()->content;
-		        $marks_per_question = Setting::where('name','marks_per_question')->first()->content;
-		        $score = (($count_correct * $marks_per_question) / $passing_score) * 100;
-		        //set data
-                $postdata['total_questions'] = $count_questions;
-                $postdata['correct'] = $count_correct;
-                $postdata['wrong'] = $count_wrong;
-                $postdata['attempted'] = $count_wrong + $count_correct;
-                $postdata['score'] = $score;
-                $postdata['status'] = $score >= $passing_score ? "Pass" : "Fail";
-                $postdata['course_id'] = Lesson::where('id',$postdata['lesson_id'])->first()->course_id;
-                unset($postdata['is_correct']);
+
+                $quiz = is_string($postdata['quiz']) ? json_decode($postdata['quiz'],true) : $postdata['quiz'];
+                //  dd($postdata);
+
+                if(count($quiz)>0) {
+
+                    $count_correct = 0;
+                    $count_wrong = 0;
+
+                    foreach($quiz as $rows){
+
+                        if($rows['is_correct'] == 1){
+                            $count_correct++;
+                        }
+                        else{
+                            $count_wrong++;
+                        }
+                    }
+
+                    $count_questions = Quiz::where('topic_id',$postdata['topic_id'])->count();
+                    $passing_score = Setting::where('name','passing_score')->first()->content;
+                    $marks_per_question = Setting::where('name','marks_per_question')->first()->content;
+                    $score = (($count_correct * $marks_per_question) / $passing_score) * 100;
+                    //set data
+                    $postdata['total_questions'] = $count_questions;
+                    $postdata['correct'] = $count_correct;
+                    $postdata['wrong'] = $count_wrong;
+                    $postdata['attempted'] = $count_wrong + $count_correct;
+                    $postdata['score'] = $score;
+                    $postdata['status'] = $score >= $passing_score ? "pass" : "fail";
+                    unset($postdata['quiz']);
+                    //This method will be execute before run the main process
+                }
+                else{
+                    $this->output(sendErrorToClient('Invalid Request'));
+                }
 
                 //This method will be execute before run the main process
 
@@ -47,9 +67,32 @@
 		    }
 
 		    public function hook_after($postdata,&$result) {
-
 		        //This method will be execute after run the main process
-
+                $response = $this->model->find($result['id']);
+                $this->output(makeClientHappy($response));
 		    }
+
+            /**
+             * @param string $output
+             * @return \Illuminate\Http\JsonResponse
+             */
+            public function execute_api($output = 'API')
+            {
+                try{
+                    $result = parent::execute_api();
+                    $api_response = isset($result->original) ? $result->original : [];
+                    $response = apiResponse($api_response);
+
+                    if($api_response['api_status'] == 1){
+                        return response()->json($response, 200);
+                    }
+                    return response()->json($response, 400);
+                }
+                catch (\Exception $e){
+                    $response['message'] = $e->getMessage();
+                    // $response['trace'] = $e->getTraceAsString();
+                    return response()->json($response,400);
+                }
+            }
 
 		}
