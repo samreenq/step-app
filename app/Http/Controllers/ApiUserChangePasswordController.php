d91cd62a887856bd1280de6ac8e79c8892df2e95
samreen <samreen.quyyum@cubixlabs.com>
2019-11-28 17:32:36 +0500

    Change Password - send access token with user

--- app/Http/Controllers/ApiUserChangePasswordController.php
+++ app/Http/Controllers/ApiUserChangePasswordController.php
@@ -18,7 +18,7 @@
 
 		    public function hook_before(&$postdata) {
 
-                $user_data = $this->user_model->where(['id'=> $postdata['user_id'], 'is_active'=>1])->first();
+                $user_data = $this->user_model->with('token')->where(['id'=> $postdata['user_id'], 'is_active'=>1])->first();
 
 				if(empty($user_data)) {
 					$this->output(sendErrorToClient('No User Found.'));
