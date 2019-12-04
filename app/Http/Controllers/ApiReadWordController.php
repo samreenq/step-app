ba381ff22eb9d204d261eeeaa8f9e9e330d1b9d7
samreen <samreen.quyyum@cubixlabs.com>
2019-12-04 16:59:36 +0500

    Update vocabulary Word

---
+++ app/Http/Controllers/ApiReadWordController.php
@@ -0,0 +1,79 @@
+<?php namespace App\Http\Controllers;
+
+		use App\ReadWords;
+        use App\VocabularyWords;
+        use Session;
+		use Request;
+		use DB;
+		use CRUDBooster;
+
+		class ApiReadWordController extends \crocodicstudio\crudbooster\controllers\ApiController {
+
+		    function __construct() {    
+				$this->table       = "read_words";        
+				$this->permalink   = "read_word";    
+				$this->method_type = "post";
+				$this->model = new ReadWords();
+		    }
+		
+
+		    public function hook_before(&$postdata) {
+		        //This method will be execute before run the main process
+                $already_read = $this->model->where('user_id',$postdata['user_id'])
+                    ->where('word_id',$postdata['word_id']);
+
+                if($already_read->count() > 0){
+                    $row = $already_read->first();
+
+                    $vocabulary_model = new VocabularyWords();
+                    $data = $vocabulary_model->getUserVocabulary($row['id'],$postdata['user_id']);
+
+                    $this->output(makeClientHappy($data));
+                }
+                else{
+                    $postdata['is_read'] = 1;
+                }
+		    }
+
+		    public function hook_query(&$query) {
+		        //This method is to customize the sql query$vocabulary_model = new VocabularyWords();
+                //                    $data = $vocabulary_model->getUserVocabulary($result['id'],$postdata['user_id']);
+
+		    }
+
+		    public function hook_after($postdata,&$result) {
+		        //This method will be execute after run the main process
+                if($result['api_status'] == 1){
+
+                    $vocabulary_model = new VocabularyWords();
+                    $data = $vocabulary_model->getUserVocabulary($result['id'],$postdata['user_id']);
+                    $this->output(makeClientHappy($data));
+                }
+
+		    }
+
+            /**
+             * @param string $output
+             * @return \Illuminate\Http\JsonResponse
+             */
+            public function execute_api($output = 'API')
+            {
+                try{
+                    $result = parent::execute_api();
+                    //echo '<pre>'; print_r($result->original); exit;
+                    $api_response = isset($result->original) ? $result->original : [];
+                    $response = apiResponse($api_response);
+
+                    if($api_response['api_status'] == 1){
+                        return response()->json($response, 200);
+                    }
+                    return response()->json($response, 400);
+                }
+                catch (\Exception $e){
+                    $response['message'] = $e->getMessage();
+                    // $response['trace'] = $e->getTraceAsString();
+                    return response()->json($response,400);
+                }
+            }
+
+		}
--- app/VocabularyWords.php
+++ app/VocabularyWords.php
@@ -3,9 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 
 class VocabularyWords extends Model
 {
-    protected $table = 'vocabulary_words';
+    public $table = 'vocabulary_words';
+
+    /**
+     * @param $word_id
+     * @param $user_id
+     * @return mixed
+     */
+    public function getUserVocabulary($word_id,$user_id)
+    {
+        return $this->select($this->table.'.*',DB::raw('IFNULL(read_words.is_read, 0) as is_read'))
+            ->leftJoin('read_words','read_words.word_id','=',$this->table.'.id')
+            ->where($this->table.'.id',$word_id)
+            ->where('read_words.user_id',$user_id)
+            ->where($this->table.'.is_active',1)
+            ->whereNull($this->table.'.deleted_at')
+            ->first();
+    }
+
+
+
+
 }
