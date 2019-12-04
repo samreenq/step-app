a2c9a85cba0da733edbd605d7813462d25c7ce59
samreen <samreen.quyyum@cubixlabs.com>
2019-12-04 15:45:25 +0500

    Vocabulary List API

---
+++ app/Http/Controllers/ApiVocabularyListController.php
@@ -0,0 +1,70 @@
+<?php namespace App\Http\Controllers;
+
+		use App\VocabularyWords;
+        use Session;
+		use Request;
+		use DB;
+		use CRUDBooster;
+
+		class ApiVocabularyListController extends \crocodicstudio\crudbooster\controllers\ApiController {
+
+		    function __construct() {    
+				$this->table       = "vocabulary_words";        
+				$this->permalink   = "vocabulary_list";    
+				$this->method_type = "get";    
+		    }
+		
+
+		    public function hook_before(&$postdata) {
+		        //This method will be execute before run the main process
+                $data = [];
+                $model = new VocabularyWords();
+                $response = $model->select($this->table.'.*',DB::raw('IFNULL(read_words.is_read, 0) as is_read'))
+                    ->leftJoin('read_words','read_words.word_id','=',$this->table.'.id')
+                    ->where($this->table.'.is_active',1)
+                    ->whereNull($this->table.'.deleted_at')
+                    ->orderBy($this->table.'.id')
+                    ->paginate(10);
+
+                if($response){
+                    // echo '<pre>'; print_r($response->toArray()); exit;
+                    $data = makeClientHappyWithPagination($response);
+                }
+                // echo '<pre>'; print_r($data); exit;
+                $this->output($data);
+		    }
+
+		    public function hook_query(&$query) {
+		        //This method is to customize the sql query
+
+		    }
+
+		    public function hook_after($postdata,&$result) {
+		        //This method will be execute after run the main process
+
+		    }
+            /**
+             * @param string $output
+             * @return \Illuminate\Http\JsonResponse
+             */
+            public function execute_api($output = 'API')
+            {
+                try{
+                    $result = parent::execute_api();
+                    //echo '<pre>'; print_r($result->original); exit;
+                    $api_response = isset($result->original) ? $result->original : [];
+                    $response = apiResponse($api_response);
+
+                    if($api_response['api_status'] == 1){
+                        return response()->json($response, 200);
+                    }
+                    return response()->json($response, 400);
+                }
+                catch (\Exception $e){
+                    $response['message'] = $e->getMessage();
+                    // $response['trace'] = $e->getTraceAsString();
+                    return response()->json($response,400);
+                }
+            }
+
+		}
---
+++ app/VocabularyWords.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class VocabularyWords extends Model
+{
+    protected $table = 'vocabulary_words';
+}
